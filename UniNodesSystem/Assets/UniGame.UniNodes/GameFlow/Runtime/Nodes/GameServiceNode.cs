4552203ebda1a21359c094a0dcdc571d45df2e29
MikhailovSergey <mikhailovss@plavalaguna.ru>
2020-02-17 22:29:34 +0300

    - fix node serialization on GameService node

--- UniNodesSystem/Assets/UniGame.UniNodes/GameFlow/Runtime/Nodes/GameServiceNode.cs
+++ UniNodesSystem/Assets/UniGame.UniNodes/GameFlow/Runtime/Nodes/GameServiceNode.cs
@@ -1,11 +1,7 @@
 ﻿namespace UniGreenModules.UniGameSystems.Runtime.Nodes
 {
-    using UniCore.Runtime.Attributes;
-    using UniCore.Runtime.Interfaces;
-    using UniCore.Runtime.ProfilerTools;
     using UniGameFlow.UniNodesSystem.Assets.UniGame.UniNodes.NodeSystem.Runtime.Attributes;
     using UniGameSystem.Runtime.Interfaces;
-    using UnityEngine;
 
     [HideNode]
     public abstract class GameServiceNode<TService> :
@@ -21,14 +17,6 @@
         where TServiceApi : IGameService
         where TService : class, TServiceApi, new()
     {
-        #region inspector
-
-        [Header("Service Status")]
-        [ReadOnlyValue]
-        [SerializeField]
-        private bool isReady;
-        
-        #endregion
  
         protected override TService CreateService() => service ?? new TService();
         
