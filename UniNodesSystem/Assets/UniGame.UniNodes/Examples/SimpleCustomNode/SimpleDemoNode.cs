77072f0a9317961711bc4d8422d893d469fb577d
MikhailovSergey <mikhailovss@plavalaguna.ru>
2020-02-18 23:54:57 +0300

    - update simple custom node demo

--- UniNodesSystem/Assets/UniGame.UniNodes/Examples/SimpleCustomNode/SimpleDemoNode.cs
+++ UniNodesSystem/Assets/UniGame.UniNodes/Examples/SimpleCustomNode/SimpleDemoNode.cs
@@ -1,8 +1,25 @@
-﻿using UniGreenModules.UniGameFlow.UniNodesSystem.Assets.UniGame.UniNodes.NodeSystem.Runtime.Nodes;
+﻿using UniGreenModules.UniGameFlow.UniNodesSystem.Assets.UniGame.UniNodes.NodeSystem.Runtime.Attributes;
+using UniGreenModules.UniGameFlow.UniNodesSystem.Assets.UniGame.UniNodes.NodeSystem.Runtime.Nodes;
+using UniGreenModules.UniGameFlow.UniNodesSystem.Assets.UniGame.UniNodes.NodeSystem.Runtime.ReactivePorts;
 using UniGreenModules.UniNodeSystem.Runtime.Core;
+using UnityEngine;
 
 [CreateNodeMenu("Examples/SimpleCustomNodes/SimpleNode",nodeName = "Simple Node")]
 public class SimpleDemoNode : UniNode
 {
+
+    [SerializeField]
+    private int intDataOne;
+    
+    public int intDataTwo;
+
+    [PortValue(PortIO.Input)]
+    public int inInt;
+
+    [PortValue(PortIO.Output)] 
+    public float outInt;
+
+    [ReactivePort()]
+    public IntReactivePort inReactiveInt;
 
 }
