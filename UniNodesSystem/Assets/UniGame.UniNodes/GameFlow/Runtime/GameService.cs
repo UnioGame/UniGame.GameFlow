c526206aed0a6e35cf13ceb5a804bcf0c056be41
MikhailovSergei <mikhailovs@taktikagames.ru>
2020-02-21 16:40:25 +0300

    - add GameService Complete method to mark service awaiter as Ready

--- UniNodesSystem/Assets/UniGame.UniNodes/GameFlow/Runtime/GameService.cs
+++ UniNodesSystem/Assets/UniGame.UniNodes/GameFlow/Runtime/GameService.cs
@@ -7,28 +7,52 @@
     using UniGreenModules.UniCore.Runtime.Interfaces;
     using UniRx;
 
+    /// <summary>
+    /// base game service class for binding Context source data to service logic
+    /// </summary>
     [Serializable]
-    public abstract class GameService : IGameService
+    public abstract class GameService : IGameService, ICompletionSource
     {
-        protected LifeTimeDefinition lifeTimeDefinition = new LifeTimeDefinition();
+        protected readonly LifeTimeDefinition lifeTimeDefinition = new LifeTimeDefinition();
+
+        protected readonly BoolReactiveProperty isReady = new BoolReactiveProperty(false);
         
-        protected BoolReactiveProperty isReady = new BoolReactiveProperty(false);
+        /// <summary>
+        /// service local lifetime
+        /// </summary>
+        protected ILifeTime serviceLifeTime;
 
         public IContext Bind(IContext context, ILifeTime lifeTime = null)
         {
-            var bindLifeTime = lifeTime ?? context.LifeTime;
-            return OnBind(context, bindLifeTime);
+            serviceLifeTime = lifeTimeDefinition.LifeTime;
+            lifeTime?.AddDispose(this);
+            return OnBind(context, serviceLifeTime);
         }
         
+        /// <summary>
+        /// complete service awaiter to mark it as ready
+        /// </summary>
+        public void Complete()
+        {
+            isReady.Value = true;
+        }
+        
+        /// <summary>
+        /// terminate service lifeTime to release resources
+        /// </summary>
         public void Dispose()
         {
             lifeTimeDefinition.Terminate();
         }
+
+        public bool IsComplete => isReady.Value;
 
         public ILifeTime LifeTime => lifeTimeDefinition.LifeTime;
 
         public IReadOnlyReactiveProperty<bool> IsReady => isReady;
 
         protected abstract IContext OnBind(IContext context, ILifeTime lifeTime);
+
+
     }
 }
