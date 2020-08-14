02b0a279d597d48e2418bb1034d239e2f9db1073
Mefodei <mefodei.link@gmail.com>
2020-08-14 13:06:17 +0300

    - fix oirtvalue initialization

--- NodeSystem/Runtime/Core/PortValue.cs
+++ NodeSystem/Runtime/Core/PortValue.cs
@@ -18,7 +18,7 @@
     using UnityEngine;
 
     [Serializable]
-    public class PortValue : IPortValue , ISerializationCallbackReceiver
+    public class PortValue : IPortValue, ISerializationCallbackReceiver
     {
         #region serialized data
 
@@ -30,11 +30,11 @@
         /// <summary>
         /// allowed port value types
         /// </summary>
-        [SerializeField] protected List<string> serializedValueTypes;
-
         [SerializeField]
-        [ReadOnlyValue] protected int broadcastersCount;
-        
+        protected List<string> serializedValueTypes;
+
+        [SerializeField] [ReadOnlyValue] protected int broadcastersCount;
+
         #endregion
 
         #region private property
@@ -46,18 +46,26 @@
         private ReactiveCommand portValueChanged = new ReactiveCommand();
 
         private ILifeTime lifeTime;
-        
+
         private LifeTimeDefinition lifeTimeDefeDefinition = new LifeTimeDefinition();
-        
+
         private List<Type> valueTypeFilter;
 
-        
-#endregion
+        #endregion
+
+        #region constructor
+
+        public PortValue()
+        {
+            Initialize();
+        }
+
+        #endregion
 
         #region public properties
-        
+
         public IReadOnlyList<Type> ValueTypes => valueTypeFilter = valueTypeFilter ?? new List<Type>();
-       
+
         public ILifeTime LifeTime => lifeTimeDefeDefinition.LifeTime;
 
         public string ItemName => name;
@@ -74,21 +82,11 @@
         }
 
         #endregion
-        
+
         public void Initialize(string portName)
         {
             name = portName;
-            
-            lifeTimeDefeDefinition = lifeTimeDefeDefinition ?? new LifeTimeDefinition();
-            lifeTime = lifeTimeDefeDefinition.LifeTime;
-            lifeTimeDefeDefinition.Release();
-            
-            data     = data ?? new TypeData();
-            broadcaster = broadcaster ?? new TypeDataBrodcaster();
-
-            lifeTime.AddCleanUpAction(data.Release);
-            lifeTime.AddCleanUpAction(RemoveAllConnections);
-            
+            Initialize();
         }
 
 
@@ -97,12 +95,12 @@
             valueTypeFilter = valueTypeFilter ?? new List<Type>();
             valueTypeFilter.Clear();
             valueTypeFilter.AddRange(types);
-            
+
             UpdateSerializedFilter(valueTypeFilter);
         }
 
         public void Dispose() => Release();
-        
+
         public void Release() => lifeTimeDefeDefinition.Terminate();
 
         #region type data container
@@ -119,16 +117,15 @@
 
         public void Publish<TData>(TData value)
         {
-            if (valueTypeFilter != null && 
-                valueTypeFilter.Count != 0  && 
+            if (valueTypeFilter != null &&
+                valueTypeFilter.Count != 0 &&
                 !valueTypeFilter.Contains(typeof(TData))) {
                 return;
             }
-            
+
             data.Publish(value);
             broadcaster.Publish(value);
             portValueChanged.Execute(Unit.Default);
-            
         }
 
         public void RemoveAllConnections() => broadcaster.Release();
@@ -147,16 +144,29 @@
         public IDisposable Bind(IMessagePublisher contextData)
         {
             var disposable = broadcaster.Bind(contextData);
-            broadcastersCount = broadcaster.ConnectionsCount;            
+            broadcastersCount = broadcaster.ConnectionsCount;
             return disposable;
         }
-        
+
         #endregion
 
         private bool DefaultFilter(Type type) => true;
 
+        private void Initialize()
+        {
+            lifeTimeDefeDefinition = lifeTimeDefeDefinition ?? new LifeTimeDefinition();
+            lifeTime               = lifeTimeDefeDefinition.LifeTime;
+            lifeTimeDefeDefinition.Release();
+
+            data        = data ?? new TypeData();
+            broadcaster = broadcaster ?? new TypeDataBrodcaster();
+
+            lifeTime.AddCleanUpAction(data.Release);
+            lifeTime.AddCleanUpAction(RemoveAllConnections);
+        }
+
         #region serialization rules
-        
+
         public void OnBeforeSerialize()
         {
             UpdateSerializedFilter(valueTypeFilter);
@@ -172,25 +182,28 @@
                 var type       = Type.GetType(typeFilter, false, true);
                 if (type != null)
                     valueTypeFilter.Add(type);
-            };
+            }
+
+            ;
         }
-        
+
         [Conditional("UNITY_EDITOR")]
         private void UpdateSerializedFilter(IReadOnlyList<Type> filter)
         {
-            serializedValueTypes = filter == null ? new List<string>() : 
-                filter.Select(x => x.AssemblyQualifiedName).ToList();
+            serializedValueTypes = filter == null ? new List<string>() : filter.Select(x => x.AssemblyQualifiedName).ToList();
         }
 
         #endregion
 
 
         #region Unity Editor Api
+
 #if UNITY_EDITOR
-        
+
         public IReadOnlyDictionary<Type, IValueContainerStatus> EditorValues => data.EditorValues;
-        
+
 #endif
+
         #endregion
     }
 }
