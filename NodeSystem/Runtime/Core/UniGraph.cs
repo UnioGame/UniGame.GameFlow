052871ca298ebf702e24ee273967376c904fc1d4
Mefodei <mefodei.link@gmail.com>
2020-10-06 03:13:02 +0300

    - fix odin dependencies

--- NodeSystem/Runtime/Core/UniGraph.cs
+++ NodeSystem/Runtime/Core/UniGraph.cs
@@ -5,12 +5,12 @@
     using Attributes;
     using Runtime.Extensions;
     using Runtime.Interfaces;
-    using Sirenix.Utilities;
     using UniGreenModules.UniContextData.Runtime.Interfaces;
     using UniGreenModules.UniCore.Runtime.Rx.Extensions;
     using UniGreenModules.UniGame.AddressableTools.Runtime.Extensions;
     using UniGreenModules.UniGame.SerializableContext.Runtime.Addressables;
     using UniModules.UniGame.Context.Runtime.Abstract;
+    using UniModules.UniGame.Core.Runtime.Extension;
     using UniModules.UniGameFlow.NodeSystem.Runtime.Extensions;
     using UniRx;
     using UnityEngine;
