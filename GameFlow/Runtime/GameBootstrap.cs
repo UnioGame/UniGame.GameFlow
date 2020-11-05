3053be064d0a2fb0761d5daf2282b4d514b16056
Mefodei <mefodei.link@gmail.com>
2020-11-05 23:04:28 +0300

    -  fix gameboot script

--- GameFlow/Runtime/GameBootstrap.cs
+++ GameFlow/Runtime/GameBootstrap.cs
@@ -5,12 +5,16 @@
 
     public static class GameBootstrap {
 
-        public const string BootLabale = "BootPoint";
+        public const string BootLabel = "BootPoint";
 
         [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterSceneLoad)]
         public static async void Initialize() {
-
-            var bootPoint   = await Addressables.LoadAssetAsync<GameObject>(BootLabale).Task;
+            
+            var bootPoint   = await Addressables.LoadAssetAsync<GameObject>(BootLabel).Task;
+            if (!bootPoint)
+            {
+                Debug.LogWarning($"Boot GameManager Not Found with label {BootLabel}");
+            }
             var gameManager = Object.Instantiate(bootPoint).GetComponent<Runtime.GameManager>();
             Object.DontDestroyOnLoad(gameManager.gameObject);
             
