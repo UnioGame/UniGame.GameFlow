f2ebbba96f1151ed76b35664aac46e34476d6c86
Mefodei <mefodei.link@gmail.com>
2020-11-17 15:45:44 +0300

    - remove old demos

--- ~Examples/ContextNodes/SimpleServices/Runtime/Nodes/Simple4Node.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace UniGame.UniNodes.Examples.ContextNodes.SimpleServices.Runtime.Nodes
-{
-    using GameFlow.Runtime.Nodes;
-    using NodeSystem.Runtime.Core;
-    using UniModules.UniGameFlow.NodeSystem.Runtime.Core.Attributes;
-
-    [CreateNodeMenu("Examples/DemoSystem/Simple4")]
-    public class Simple4Node : GameServiceNode<SimpleSystem4> { }
-}
