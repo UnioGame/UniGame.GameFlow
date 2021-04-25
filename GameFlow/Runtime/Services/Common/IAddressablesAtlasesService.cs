a0cebca55e8ee99ef60f527593d68c8064923f07
Mefodei <mefodei.link@gmail.com>
2021-04-25 23:56:35 +0300

    update packages

--- GameFlow/Runtime/Services/Common/IAddressablesAtlasesService.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace UniModules.UniGameFlow.GameFlow.Runtime.Services.Common
-{
-    using Interfaces;
-    using UniGame.AddressableTools.Runtime.SpriteAtlases.Abstract;
-
-    public interface IAddressablesAtlasesService : IGameService, IAddressablesAtlasesLoader
-    {
-    }
-}
