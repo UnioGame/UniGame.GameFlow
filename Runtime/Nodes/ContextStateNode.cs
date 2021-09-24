4101e838e2c05a80daf4adb36d50b3e76eae19c0
MikhailovSergei <mikhailovs@taktikagames.ru>
2021-09-24 11:55:13 +0300

    - add context state node

---
+++ Runtime/Nodes/ContextStateNode.cs
@@ -0,0 +1,45 @@
+namespace Game.Modules.Assets.UniGame.GameFlow.Runtime.Nodes
+{
+    using Cysharp.Threading.Tasks;
+    using global::UniGame.UniNodes.Nodes.Runtime.Common;
+    using UniCore.Runtime.ProfilerTools;
+    using UniModules.UniGame.Context.SerializableContext.Runtime.Abstract;
+    using UniModules.UniGame.Core.Runtime.Interfaces;
+    using UniModules.UniGameFlow.NodeSystem.Runtime.Core.Attributes;
+    using UnityEngine;
+
+    [CreateNodeMenu("UniGame/ContextStateNode")]
+    public class ContextStateNode : ContextNode
+    {
+
+        [SerializeReference]
+        public IAsyncContextState contextState;
+
+        public bool completeOnFalse = true;
+        
+        protected sealed override async UniTask OnContextActivate(IContext context)
+        {
+#if UNITY_EDITOR
+            if (contextState == null)
+            {
+                GameLog.LogError($"NODE: {nameof(ContextStateNode)} : {name} contextState is NULL");
+                return;
+            }
+#endif
+
+            var result = await contextState.ExecuteAsync(context);
+
+            switch (result)
+            {
+                case AsyncStatus.Succeeded:
+                case {} when completeOnFalse == true:
+                    Complete();
+                    break;
+                default:
+                    GameLog.LogError($"NODE: {nameof(ContextStateNode)} : {name} STATUS {result}");
+                    break;
+            }
+            
+        }
+    }
+}
